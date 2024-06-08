e3d6bda50f949fc92ac3b48d5fa626efc8f71141
purwostwn <purwostwn98@gmail.com>
2024-06-08 23:20:38 +0700

    a

--- app/Views/depan/profil.php
+++ app/Views/depan/profil.php
@@ -5,10 +5,10 @@
 <div class="page-title" data-aos="fade">
   <div class="heading">
     <div class="container">
-      <div class="row d-flex justify-content-center text-center">
-        <div class="col-lg-8">
-          <h1>Profil Puskesmas Kuok</h1>
-          <p class="mb-0">UPT Puskesmas Kuok merupakan satu-satunya Puskesmas induk di Kecamatan Kuok dan UPT Puskesmas Kuok berada di wilayah Desa Lereng. </p>
+      <div class="row d-flex justify-content-center ">
+        <div class="col-lg-12">
+          <h1 class="text-center">Profil Puskesmas Kuok</h1>
+          <p class="mb-0 text-left">UPT Puskesmas Kuok merupakan satu-satunya Puskesmas induk di Kecamatan Kuok dan UPT Puskesmas Kuok berada di wilayah Desa Lereng. </p>
           <p class="mb-0">UPT Puskesmas Kuok awalnya dibangun sesuai standar puskesmas non rawat satu lantai pada tahun 1983 - 1984 yang kemudian mengalami renovasi dan penambahan gedung. Kemudian dijadikan Puskesmas Rawat Inap dan Puskesmas PONED dengan Izin Operasional Puskesmas Nomor : 440 – 423/V/2022.</p>
         </div>
       </div>
